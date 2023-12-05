dc742ce3728a3dba361eb26b072f6cc45f3f99d6
Jinjin21 <64459644+Jinjin21@users.noreply.github.com>
2023-12-05 10:54:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript
+
+//実行する機能
+const show = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const keyframes = {
+        opacity: [0, 1],
+        translate: ['150px 0', 0]
+      }
+      entries[0].target.animate(keyframes, 1000);
+      //一度表示されたら止める
+      obs.unobserve(entry.target);
+    }
+  });
+}
+
+//監視機能の設置
+const observer = new IntersectionObserver(show);
+
+//監視対象
+const images = document.querySelectorAll('.img');
+images.forEach(img => {
+  observer.observe(img);
+});
